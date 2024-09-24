a85f042d8c580046751b10e0a6805dfa8088839c
Lucas Crussiere <lcrussiere@iutbayonne.univ-pau.fr>
2024-09-24 16:49:13 +0200

    Création du projet

---
+++ src/main/java/com/lcrussiere/calculator/Calculator.java
@@ -0,0 +1,4 @@
+package com.lcrussiere.calculator;
+
+public class Calculator {
+}
---
+++ src/main/java/com/lcrussiere/calculator/Main.java
@@ -0,0 +1,10 @@
+package com.lcrussiere.calculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+
