ca6695bf6df8b9109342b0448cd08b3295970b74
Lucas Crussiere <lcrussiere@iutbayonne.univ-pau.fr>
2024-09-26 15:56:48 +0200

    BeforeEach et AfterEach

--- src/test/java/com/lcrussiere/calculatortest/CalculatorTest.java
+++ src/test/java/com/lcrussiere/calculatortest/CalculatorTest.java
@@ -1,24 +1,42 @@
 package com.lcrussiere.calculatortest;
 
 import com.lcrussiere.calculator.Calculator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
+    Integer opG;
+    Integer opD;
+
+    @BeforeEach
+    void setup(){
+        opG = 2;
+        opD = 3;
+    }
+
+
     @Test
     public void testAdd() {
         Calculator calculator = new Calculator();
-        assertEquals(5, calculator.add(2, 3));
-        assertEquals(-1, calculator.add(-2, 1));
-        assertEquals(0, calculator.add(0, 0));
+        // Calculer la somme de opG et opD
+        int result = calculator.add(opG, opD);
+        assertEquals(result, calculator.add(opG, opD));
     }
 
     @Test
     public void testDivide() {
         Calculator calculator = new Calculator();
-        assertEquals(2, calculator.divide(6, 3));
-        assertEquals(-2, calculator.divide(-6, 3));
-        assertEquals(0, calculator.divide(0, 1));
+        // Calculer la division de opG par opD
+        int result = calculator.divide(opG, opD);
+        assertEquals(result, calculator.divide(opG, opD));
+    }
+
+    @AfterEach
+    void tearDown(){
+        opG = null;
+        opD = null;
     }
 }
